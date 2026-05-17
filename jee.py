3818f3830858b88ca65f73e9d95caedc3524f44f
RISHABH GARG <gargrg0610@gmail.com>
2026-05-17 11:15:54 +0530

    Initial commit: Add JEE marks data and analysis code

---
+++ jee.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+data=pd.read_csv("jee_marks_percentile_rank_2009_2026.csv")
+print(data.head())
+print()
