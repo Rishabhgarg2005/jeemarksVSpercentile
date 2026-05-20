226a600d9cf882f6daeb17f64a5cf820a85b8764
rishabhgarg2005 <your-email@example.com>
2026-05-21 00:02:47 +0530

    new

--- jee.py
+++ jee.py
@@ -6,3 +6,5 @@ import seaborn as sns
 data=pd.read_csv("jee_marks_percentile_rank_2009_2026.csv")
 print(data.head())
 print(data.isna().sum())
+data =data.drop(columns ="Total_Candidates")
+print(data.head())
